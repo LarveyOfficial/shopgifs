53d05468cb79ef12f01cdafac1fa32d05b4a02b6
Larvey <luisrvervaet@gmail.com>
2023-04-29 14:47:16 -0400

    New Favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import Head from "next/head";
 
 export default function App({
   Component,
@@ -8,6 +9,30 @@ export default function App({
 }: AppProps) {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <title>Shop Gifs</title>
+        <meta name="description" content="Gifs player for the MTU OPS Shop" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#eab308" />
+        <meta name="msapplication-TileColor" content="#eab308" />
+        <meta name="theme-color" content="#374151"></meta>
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   );
