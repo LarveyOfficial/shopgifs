03e356c01496b87028ad1f0e8453f08f3d28961d
Larvey <luisrvervaet@gmail.com>
2023-04-29 00:22:59 -0400

    Moved stuff

--- src/pages/panel/home.tsx
+++ src/pages/panel/home.tsx
@@ -10,11 +10,8 @@ export function Home() {
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight dark:text-white">
-              Hello {session.user!.name}!
+              This is the home page :D
             </h2>
-            <h3 className="text-center dark:text-slate-400">
-              {session.user!.email}
-            </h3>
             <br />
           </div>
         </div>
