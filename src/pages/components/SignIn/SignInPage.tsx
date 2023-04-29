e229129f91df0269ed1ecaf8451c7ebcba6a4992
Larvey <luisrvervaet@gmail.com>
2023-04-29 00:29:57 -0400

    its 12 am why am I still working on this

--- src/pages/components/SignIn/SignInPage.tsx
+++ src/pages/components/SignIn/SignInPage.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { signIn } from "next-auth/react";
 export function SignInPage() {
   return (
@@ -16,31 +17,39 @@ export function SignInPage() {
             Sign in to access the Panel
           </h2>
           <br />
-          <button
-            onClick={() =>
-              signIn("google", {
-                callbackUrl: `${window.location.origin}/panel/home`,
-              })
-            }
-            className="text-dark flex w-full justify-center rounded-md bg-yellow-500 px-3 py-1.5 text-sm font-semibold leading-6 shadow-sm hover:bg-yellow-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-600"
-          >
-            <svg
-              className="my-1 -ml-1 mr-2 h-4 w-4"
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fab"
-              data-icon="google"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 488 512"
+          <div className="flex-auto space-y-3">
+            <button
+              onClick={() =>
+                signIn("google", {
+                  callbackUrl: `${window.location.origin}/panel/home`,
+                })
+              }
+              className="text-dark flex w-full justify-center rounded-md bg-yellow-500 px-3 py-1.5 text-sm font-semibold leading-6 shadow-sm hover:bg-yellow-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-600"
             >
-              <path
-                fill="currentColor"
-                d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
-              ></path>
-            </svg>
-            <span>Sign in with Google</span>
-          </button>
+              <svg
+                className="my-1 -ml-1 mr-2 h-4 w-4"
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fab"
+                data-icon="google"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 488 512"
+              >
+                <path
+                  fill="currentColor"
+                  d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
+                ></path>
+              </svg>
+              <span>Sign in with Google</span>
+            </button>
+            <Link
+              href="/"
+              className="text-dark flex w-full justify-center rounded-md bg-blue-500 px-3 py-1.5 text-sm font-semibold leading-6 shadow-sm hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+            >
+              <span>Go to GIF Player</span>
+            </Link>
+          </div>
         </div>
       </div>
     </>
