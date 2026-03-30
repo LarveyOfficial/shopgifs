b15f6279c1889af89eb77e9c0a2e8cdcf7975640
Luis Vervaet <luisvervaet@gmail.com>
2026-03-30 11:04:41 -0400

    Update users.tsx

--- src/pages/panel/users.tsx
+++ src/pages/panel/users.tsx
@@ -235,7 +235,7 @@ export function Users() {
                       />
                     </td>
                     <td className="whitespace-nowrap p-3 text-center text-sm text-white">
-                      <div className="relative inline-flex cursor-pointer items-center">
+                      <label className="relative inline-flex cursor-pointer items-center">
                         <input
                           type="checkbox"
                           id="admin"
@@ -246,7 +246,7 @@ export function Users() {
                           checked={formData.admin == "true"}
                         />
                         <div className="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:left-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-800 dark:peer-focus:ring-blue-800" />
-                      </div>
+                      </label>
                     </td>
                     <td className="whitespace-nowrap p-3 text-center text-sm text-white">
                       <form onSubmit={handleSubmit}>
